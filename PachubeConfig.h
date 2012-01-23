cf78edbe536ac49cda77efedec1bad474eaedfcb
MarsFlyer <Paul_T_Allen@yahoo.com>
2012-01-23 19:11:26 +0000

    Without Pachube API.

--- PachubeConfig.h
+++ PachubeConfig.h
@@ -1,8 +1,11 @@
 #ifndef PACHUBE_CONFIG_H_
 #define PACHUBE_CONFIG_H_
 
-define FEEDPUT       99999 //post to this feed (this has to be your own feed)
+#define FEEDPUT       1234 //post to this feed (this has to be your own feed)
 //#define FEEDGET     8281  //get from this feed 
-#define APIKEY         "xxxx" // replace your pachube api key here
+        		// replace your pachube api key here
+#define APIKEY1       "abc"
+#define APIKEY2       "def" 
+       		// Print doesn't like strings greater than 128!
 
 #endif // PACHUBE_CONFIG_H_
